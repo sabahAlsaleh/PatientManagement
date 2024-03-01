17434b57a0879cf95db45e46e7dba07be39e010f
Sabah <sabahaa@kth.se>
2024-03-01 10:32:22 +0100

    edit

--- src/test/java/kth/system/healthcare/PatientRegistrationService/PatientManagementApplicationTests.java
+++ src/test/java/kth/system/healthcare/PatientRegistrationService/PatientManagementApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PatientManagementApplicationTests {
 
-//	@Test
-//	void contextLoads() {
-//	}
+	@Test
+	void contextLoads() {
+	}
 
 }
